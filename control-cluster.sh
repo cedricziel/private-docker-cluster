7da13cf57ee6808eb06df96e12e5c7d8ae5ad9ff
Cedric Ziel <cedric@cedric-ziel.com>
2016-01-23 23:19:48 +0100

    Print environment for controlling the cluster.

---
+++ control-cluster.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+machinePrefix="";
+swarmId="";
+provider="";
+
+if [ -f ".env" ]; then
+  source .env;
+else
+  echo "Error. No configuration available. Create a .env file from .env.example";
+  exit 1;
+fi
+
+provider=$DOCKER_MACHINE_DRIVER;
+
+if [ -f ".machine-prefix" ]; then
+  machinePrefix=$(<.machine-prefix);
+else
+  echo "Error. No machine prefix generated. Re-Run create-swarm.sh";
+  exit 1;
+fi
+
+if [ -f ".swarm-id" ]; then
+  swarmId=$(<.swarm-id);
+else
+  echo "Error. No swarm id present. Re-Run create-swarm.sh";
+  exit 1;
+fi
+
+echo "You are going to control the currently present cluster, execute";
+echo "$(docker-machine env $machinePrefix-swarm-master)";
