9e7adb6d994a6234ef6586eb58cc2b0e69556ba9
Cedric Ziel <cedric@cedric-ziel.com>
2016-01-23 22:43:08 +0100

    Create swarm master correctly

--- create-swarm-master.sh
+++ create-swarm-master.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+machinePrefix="";
+
 if [ -f ".env" ]; then
   source .env;
 else
@@ -9,10 +11,16 @@ fi
 
 provider=$DOCKER_MACHINE_DRIVER;
 
+if [ -f ".machine-prefix" ]; then
+  machinePrefix=$(<.machine-prefix);
+else
+  echo "Error. No machine prefix generated. Re-Run create-swarm.sh";
+fi
+
 echo "Creating swarm master with provider $provider";
 docker-machine create \
         -d $provider \
         --swarm \
         --swarm-master \
-        --swarm-discovery token://$swarmId \
-        swarm-master
+        --swarm-discovery "token://$swarmId" \
+        "$machinePrefix-swarm-master"
